47ff153b2f249d2c6b191644a46039c96b3c9577
santiagazo <santiagazo@gmail.com>
2025-01-21 14:27:51 -0700

    Adding outDir to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path';
-import { fileURLToPath } from 'url';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: "/",
   plugins: [react()],
+  build: {
+    outDir: "dist", // Output directory for production builds
+    assetsDir: "assets", // Directory for static assets
+  },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), // Alias '@' points to 'src'
+      "@": path.resolve(__dirname, "./src"), // Alias '@' points to 'src'
     },
   },
-})
+});
